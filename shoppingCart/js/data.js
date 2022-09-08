05d8416f1cf6804c90cc94785c53862372f932b2
Maoyori11 <maoyori11@gmail.com>
2022-09-08 17:37:34 -0500

    esto me pasa por no usar git

--- shoppingCart/js/data.js
+++ shoppingCart/js/data.js
@@ -2,7 +2,7 @@ export const dataTG = [
     {
         id: 1,
         name: "Xbox Series S+1",
-        urlImages: "./images/xbox.jpg",
+        urlImages: "./shoppingCart/images/xbox.jpg",
         stock: 8,
         subtitle:"Características",
         descriction: "-La consola Xbox Series S, totalmente digital. -Miles de juegos de 4 generaciones, con retrocompatibilidad -Velocidad y el rendimiento con la Xbox Velocity Arquitechture -Tecnología de aceleración de DirectX Raytracingle da a tus juegos un mayor nivel de realismo. -Con Xbox Entrega inteligente juega la mejor versión disponible de tu juego.",
@@ -11,7 +11,7 @@ export const dataTG = [
     {
         id: 2,
         name: "PS5 Estándar 825GB",
-        urlImages: "./images/play.jpg",
+        urlImages: "./shoppingCart/images/play.jpg",
         stock: 11,
         subtitle:"Características",
         descriction:"-Disfruta las características innovadoras de Horizon Forbidden West para PS5™ que incluye impresionante resolución 4K y HDR. -Código para descargar el juego Horizon Forbidden West. -Retrocompatibilidad juega a una selección de títulos de PS4.-Disfruta de velocidades más altas y fluidas en PS4 y PS VR. -Consola PS5 permite que los jugadores actualicen sus juegos.",
@@ -20,7 +20,7 @@ export const dataTG = [
     {
         id: 3,
         name: "NINTENDO SWITCH",
-        urlImages: "./images/nintendo.jpg",
+        urlImages: "./shoppingCart/images/nintendo.jpg",
         stock: 7,
         subtitle:"Características",
         descriction:"-En el modo portátil, hasta ocho jugadores pueden conectarse para una partida multijugador local.-Si estás jugando en la TV o en el modo sobremesa de manera local, hasta cuatro jugadores pueden disfrutar de la acción multijugador en pantalla partida. -Una consola para cada miembro de tu familia. -Opciones de juego multijugador, Modo local -Compatible con todos los juegos físicos y digitales.",
@@ -29,7 +29,7 @@ export const dataTG = [
     {
         id: 4,
         name: "ONE Fifa 22",
-        urlImages: "./images/xbox_game.jpg",
+        urlImages: "./shoppingCart/images/xbox_game.jpg",
         stock: 3,
         subtitle:"Características",
         descriction:"-Crea un club, llévalo desde los puestos de descenso hasta lo más alto de la competición. -Expresa tu estilo en el terreno de juego para que conectes con tu equipo en pistas callejeras en el mundo. -FUT 22 rediseña Division Rivals y FUT Champions, ahora lo tienes más fácil para poner a prueba tus habilidades. -Experiencia más realista del deporte, que incluye las estrellas y los equipos de futbol más importantes.",
@@ -38,7 +38,7 @@ export const dataTG = [
     {
         id: 5,
         name: "God Of War",
-        urlImages: "./images/play_game.jpg",
+        urlImages: "./shoppingCart/images/play_game.jpg",
         stock: 15,
         subtitle:"Características",
         descriction:"-Cámara a la altura de los hombros para acercar la acción. -Las luchas son similares al panteón de criaturas nórdica. -Entorno nuevo con panteón de criaturas, monstruos y dioses.",
@@ -48,7 +48,7 @@ export const dataTG = [
     {
         id: 6,
         name: "Donkey Kong Country",
-        urlImages: "./images/nintendo_game.jpg",
+        urlImages: "./shoppingCart/images/nintendo_game.jpg",
         stock: 5,
         subtitle:"Características",
         descriction:"-Características destacadas. -Con circuitos y personajes populares además de los descargables. -Te permite partidas multijugador local hasta para 8 pilotos. -Pilotos inexpertos cuenta con asistencia del nuevo volante inteligente.",
